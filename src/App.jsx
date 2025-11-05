8cad0877104030295c5c988dc6aaf73492fa8a17
Allen Saji <allensaji012@gmail.com>
2025-11-05 20:44:16 +1000

    added themes to index.css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export const App = () => {
+    return (
+        <main>
+            <div>
+                <h1>welcome to my portfolio</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App
